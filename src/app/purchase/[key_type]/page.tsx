5adfabb914e95406c7e6be0523d36fd11cafe30c
rbxlscripts <104306541+rbxlscripts@users.noreply.github.com>
2024-10-25 18:29:56 +0200

    make tos page

--- src/app/purchase/[key_type]/page.tsx
+++ src/app/purchase/[key_type]/page.tsx
@@ -4,12 +4,8 @@ const products = {
     "lifetime": "https://mspaint.sell.app/product/mspaint-lifetime-key?store=mspaint&quantity=1&data=faq"
 }
 
-export default function Page({params}: {params: {key_type: string}}) {
-    let redirectURI = "/";
-
-    if (params.key_type == "lifetime") {
-        redirectURI = products[params.key_type] as string;
-    }
-
+export default function Page({ params }: { params: { key_type: string } }) {
+    let redirectURI = "/shop";
+    if (params.key_type in products) redirectURI = products[params.key_type as keyof typeof products] as string;
     return redirect(redirectURI);
 }
--- src/app/tos/page.tsx
+++ src/app/tos/page.tsx
@@ -18,10 +18,58 @@ By purchasing and using mspaint products, you agree to the following Terms of Se
 > **Violation of any of these terms will result in a permanent ban from all current and future mspaint products.**
 */
 
+import BlurFade from "@/components/magicui/blur-fade";
+import DotPattern from "@/components/magicui/dot-pattern";
+import { cn } from "@/lib/utils";
+
 export default function TOS(){
     return (
-        <main>
-            <h1 className="text-3xl"><strong>mspaint Terms of Service</strong></h1>
+        <main className="overflow-x-hidden">
+            <DotPattern
+                width={20}
+                height={20}
+                cx={1}
+                cy={1}
+                cr={1}
+                className={cn(
+                    "[mask-image:linear-gradient(to_bottom_right,white,transparent,transparent)] -z-50",
+                )}
+            />
+    
+            <div className="flex flex-col items-center justify-center px-2 ml-5 mr-5">
+                <BlurFade delay={0.2 + (1 * 0.05)} className="mt-5" inView>
+                    <h1 className="text-3xl font-bold text-center flex flex-col">
+                        <strong>mspaint Terms of Service</strong>
+                    </h1>
+                </BlurFade>
+
+                <BlurFade delay={0.4 + (1 * 0.05)} className="mb-5 mt-2" inView>
+                    <p className="text-center">By purchasing and using mspaint products, you agree to the following Terms of Service. Violating any of these terms will result in <strong>permanent blacklisting</strong> from all mspaint products.</p>
+
+                    {/* Usage and Distribution */}
+                    <h2 className="text-2xl font-bold flex flex-col mt-6"><strong>Usage and Distribution</strong></h2>
+                    <div className="flex flex-col *:ml-5">
+                        <p>- <strong>No Resale</strong>: You may not resell any mspaint product(s) without express permission.</p>
+                        <p>- <strong>No Paywalling</strong>: You are prohibited from placing any mspaint product(s) behind a paywall or charging others for access to our products.</p>
+                        <p>- <strong>No Key Sharing</strong>: Do not share your license keys with others.</p>
+                    </div>
+                
+                    {/* Refunds and Chargebacks */}
+                    <h2 className="text-2xl font-bold flex flex-col mt-6"><strong>Refunds and Chargebacks</strong></h2>
+                    <div className="flex flex-col *:ml-5">
+                        <p>- <strong>Final Sales</strong>: All purchases are <strong>final</strong>. <code>mspaint does not offer refunds for any reason.</code></p>
+                        <p>- <strong>No Chargebacks</strong>: Chargebacks are not permitted under any circumstances. <strong>Initiating a chargeback will lead to a permanent ban.</strong></p>
+                    </div>
+
+                    {/* Tampering and Security */}
+                    <h2 className="text-2xl font-bold flex flex-col mt-6"><strong>Tampering and Security</strong></h2>
+                    <div className="flex flex-col *:ml-5">
+                        <p>- <strong>No Tampering or Cracking</strong>: You may not deobfuscate, reverse-engineer, tamper with, or crack mspaint products. Any attempt to modify or bypass product protections is strictly prohibited.</p>
+                    </div>
+
+                    <p className="text-center text-bold mt-5">Violation of any of these terms will result in a permanent ban from all current and future mspaint products.</p>
+                </BlurFade>
+            </div>
         </main>
     )
 }
