400eb6a77534bd7ca5e20c6ea53733d3ac792c89
rbxlscripts <104306541+rbxlscripts@users.noreply.github.com>
2024-10-26 11:45:28 +0200

    fix: unused variable

--- src/app/key/page.tsx
+++ src/app/key/page.tsx
@@ -9,8 +9,6 @@ import Link from "next/link";
 import { useRouter } from "next/navigation"
 
 export default function Page() {
-    const router = useRouter();
-
     return (
         <main className="relative container">
             <DotPattern
