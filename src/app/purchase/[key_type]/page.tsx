a5db326b3ac2aa07633e8f7af45faea030c410af
rbxlscripts <104306541+rbxlscripts@users.noreply.github.com>
2024-10-26 11:06:46 +0200

    fix: build

--- src/app/purchase/[key_type]/page.tsx
+++ src/app/purchase/[key_type]/page.tsx
@@ -7,5 +7,6 @@ const products = {
 export default function Page({ params }: { params: { key_type: string } }) {
     let redirectURI = "/shop";
     if (params.key_type in products) redirectURI = products[params.key_type as keyof typeof products] as string;
+    
     return redirect(redirectURI);
 }
