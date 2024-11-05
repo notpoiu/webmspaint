44e1e030a02acb6b2eafe2907305527137b11db8
upio <75510171+notpoiu@users.noreply.github.com>
2024-11-04 22:05:48 -0500

    remvoe: ajsdfkljaskldfjklasdjkfjalskdf

--- src/app/api/webhook/test/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest } from "next/server";
-
-export async function POST(request: NextRequest) {
-    const data = await request.json();
-    console.log("webhook sillyware :DDD");
-    console.log(JSON.stringify(data, null, 2));
-    return new Response("really sillyware :DDD");
-}
