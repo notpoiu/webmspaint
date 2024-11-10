8aa332fd81426da7352fd08d1beac8392eb31eb2
upio <75510171+notpoiu@users.noreply.github.com>
2024-11-10 10:31:29 -0500

    feat: purchase compelteded

--- src/app/purchase/completed/rbxl/page.tsx
+++ src/app/purchase/completed/rbxl/page.tsx
@@ -1,29 +1,5 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import Link from "next/link";
-import { useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-    const [key, setKey] = useState<string | null>(null);
-    
-    return (
-        <main className="flex justify-center items-center w-screen h-screen">
-            <Card>
-                <CardHeader>
-                    <CardTitle>mspaint - Robux Purchase</CardTitle>
-                </CardHeader>
-                <CardContent>
-                    <div className="flex flex-row gap-2">
-                        <Input placeholder="Enter key here..." value={key ?? ""} onChange={(event) => setKey(event.target.value)}/>
-                        <Link href={`https://mspaint.upio.dev/purchase/completed?serial=${encodeURIComponent(key ?? "abc")}`}>
-                            <Button>Redeem Key</Button>
-                        </Link>
-                    </div>                    
-                </CardContent>
-            </Card>
-        </main>
-    )
+    return redirect("/purchase/completed/redeem");
 }
