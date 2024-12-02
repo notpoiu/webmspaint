e12707dd9f0c17840a3847666a970f708d6216fe
mstudio45 <dadaneubert@gmail.com>
2024-12-02 21:59:37 +0100

    feat: match httpbin

--- src/app/api/useragent/route.ts
+++ src/app/api/useragent/route.ts
@@ -2,9 +2,5 @@ import { NextRequest, userAgent } from "next/server";
 
 export async function GET(request: NextRequest) {
     const { ua } = userAgent(request);
-    return new Response(ua, {
-        headers: {
-            "Content-Type": "text/plain; charset=utf-8"
-        }
-    });
+    return Response.json({ "user-agent": ua });
 }
