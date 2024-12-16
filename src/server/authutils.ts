4208c2aeae2ad2fd4e1b9bec7b04b72b3b3405f6
upio <75510171+notpoiu@users.noreply.github.com>
2024-12-16 12:01:44 -0500

    fix: revoke deivid permission to access mspaint dashboard

--- src/server/authutils.ts
+++ src/server/authutils.ts
@@ -6,7 +6,7 @@ export async function userDiscordSignOut() {
     await signOut();
 }
 
-const allowed_users = ["1177722124035706931", "389792019360514048", "1098248637789786165", "773212482835710032"];
+const allowed_users = ["1177722124035706931", "389792019360514048", "1098248637789786165"];
 export async function isUserAllowedOnDashboard() {
     const session = await auth();
 
