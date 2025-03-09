e3507213e95315fdda17aa36aaebddcdb528be6e
upio <75510171+notpoiu@users.noreply.github.com>
2025-03-09 18:02:57 -0400

    fix: mobile icons looking streched

--- src/components/executor.tsx
+++ src/components/executor.tsx
@@ -7,7 +7,7 @@ export default function Executor({ url, name, image, isWidth }: { url: string, n
     return (
     <Link href={url} target="_blank">
         <BlurFade delay={0.2 + (3 * 0.05)} className="flex flex-row items-center justify-center gap-1 max-md:gap-3" inView>
-            <Image alt="" src={image} width={isWidth == true ? 80 : 30} height={30} className={cn(`max-md:w-${isWidth == true ? "18" : "14"} max-md:h-14`)} />
+            <Image alt="" src={image} width={isWidth == true ? 80 : 30} height={30} className={cn(`max-md:w-${isWidth == true ? "18" : "14"} max-md:h-14 object-contain`)} />
             <span className="text-lg font-bold max-md:text-3xl">{name}</span>
         </BlurFade>
     </Link>);
