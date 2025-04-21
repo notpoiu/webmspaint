be63605abe725e025cfd073be89ec1d2e6295441
upio <75510171+notpoiu@users.noreply.github.com>
2025-04-21 15:57:57 -0400

    feat: presets and dynamic UUID  generation for reselling purposes

--- src/app/(dashboard)/dashboard/serials/create/components/create_key.tsx
+++ src/app/(dashboard)/dashboard/serials/create/components/create_key.tsx
@@ -2,16 +2,25 @@
 
 import CopyButton from "@/components/copy-button";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { GenerateSerial } from "@/server/redeemkey";
@@ -20,127 +29,217 @@ import { useState } from "react";
 import { toast } from "sonner";
 
 interface error_props {
-    status: number;
-    error: string;
+  status: number;
+  error: string;
 }
 
 export default function CreateSerialKey() {
-    const [new_key, setNewKey] = useState<string[] | null>(null);
-    const [is_open, setIsOpen] = useState<boolean>(false);
+  const [new_key, setNewKey] = useState<string[] | null>(null);
+  const [is_open, setIsOpen] = useState<boolean>(false);
 
-    const [amount, setAmount] = useState<number>(1);
-    const [order_id, setOrderID] = useState<string | null>(null);
+  const [amount, setAmount] = useState<number>(1);
+  const [order_id, setOrderID] = useState<string | null>(null);
 
-    return (
+  return (
+    <AlertDialog open={is_open}>
+      <AlertDialogTrigger asChild>
+        <Button
+          variant="outline"
+          onClick={() => {
+            if (!is_open) {
+              setIsOpen(true);
+            }
+          }}
+        >
+          Create Key
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          {new_key ? (
+            <>
+              <AlertDialogTitle>
+                Serial Generated Successfully!
+              </AlertDialogTitle>
+              <AlertDialogDescription>
+                Woah so cool :content: with order id:{" "}
+                {order_id ?? "REPLACEMENT"}
+              </AlertDialogDescription>
 
-        <AlertDialog open={is_open}>
-            <AlertDialogTrigger asChild>
-                <Button variant="outline" onClick={() => {
-                    if (!is_open) {
-                        setIsOpen(true);
-                    }
-                }}>Create Key</Button>
-            </AlertDialogTrigger>
-            <AlertDialogContent>
-            <AlertDialogHeader>
-                {new_key ? (
-                    <>
-                        <AlertDialogTitle>Serial Generated Successfully!</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            Woah so cool :content: with order id: {order_id ?? "REPLACEMENT"}
-                        </AlertDialogDescription>
+              {new_key.map((key, index) => (
+                <div key={index} className="flex flex-row gap-2">
+                  <Input
+                    readOnly
+                    value={`https://www.mspaint.cc/purchase/completed?serial=${encodeURIComponent(
+                      key
+                    )}`}
+                  />
+                  <CopyButton
+                    text={`https://www.mspaint.cc/purchase/completed?serial=${encodeURIComponent(
+                      key
+                    )}`}
+                  />
+                </div>
+              ))}
+            </>
+          ) : (
+            <>
+              <AlertDialogTitle>Serial Creation</AlertDialogTitle>
+              <AlertDialogDescription>
+                You sure you want to create a new serial key lil bro?
+              </AlertDialogDescription>
 
-                        {new_key.map((key, index) => (
-                            <div key={index} className="flex flex-row gap-2">
-                                <Input readOnly value={`https://www.mspaint.cc/purchase/completed?serial=${encodeURIComponent(key)}`}/>
-                                <CopyButton text={`https://www.mspaint.cc/purchase/completed?serial=${encodeURIComponent(key)}`} />
-                            </div>
-                        ))}
-                    </>
-                ) : (
-                    <>
-                        <AlertDialogTitle>Serial Creation</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            You sure you want to create a new serial key lil bro?
-                        </AlertDialogDescription>
+              <div className="flex flex-row gap-2">
+                <Input
+                  type="text"
+                  placeholder="Order ID (optional)"
+                  value={order_id ?? ""}
+                  onChange={(e) => setOrderID(e.target.value)}
+                  className="min-w-[75%] text-[16px]"
+                />
 
-                        <div className="flex flex-row gap-2">
-                            <Input
-                                type="text"
-                                placeholder="Order ID (optional)"
-                                onChange={(e) => setOrderID(e.target.value)}
-                                className="min-w-[75%] text-[16px]"
-                            />
+                <Input
+                  type="number"
+                  placeholder="Amount"
+                  onChange={(e) => setAmount(parseInt(e.target.value))}
+                  value={amount}
+                  className="max-w-[25%] text-[16px]"
+                />
+              </div>
+            </>
+          )}
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          {new_key ? (
+            <>
+              <Button
+                onClick={() => {
+                  let serial = "";
 
-                            <Input
-                                type="number"
-                                placeholder="Amount"
-                                onChange={(e) => setAmount(parseInt(e.target.value))}
-                                value={amount}
-                                className="max-w-[25%] text-[16px]"
-                            />
-                        </div>
-                    </>
-                )}
-            </AlertDialogHeader>
-            <AlertDialogFooter>
-                {new_key ? (
-                    <>
-                        <Button onClick={() => {
-                            let serial = "";
+                  for (let i = 0; i < new_key.length; i++) {
+                    serial += `https://www.mspaint.cc/purchase/completed?serial=${new_key[i]}\n`;
+                  }
 
-                            for (let i = 0; i < new_key.length; i++) {
-                                serial += `https://www.mspaint.cc/purchase/completed?serial=${new_key[i]}\n`;
-                            }
+                  const blob = new Blob([serial], { type: "text/plain" });
+                  const url = URL.createObjectURL(blob);
+                  const a = document.createElement("a");
+                  a.href = url;
+                  a.download = "serials.txt";
+                  a.click();
+                  URL.revokeObjectURL(url);
+                  toast.success("Serial key downloaded!");
+                }}
+                className="mb-2"
+                variant={"secondary"}
+              >
+                Download Serials <DownloadIcon />
+              </Button>
+              <AlertDialogCancel
+                onClick={() => {
+                  setIsOpen(false);
+                  setNewKey(null);
+                  setOrderID(null);
+                }}
+              >
+                Dismiss
+              </AlertDialogCancel>
+            </>
+          ) : (
+            <>
+              <Select
+                defaultValue="none"
+                onValueChange={(
+                  selected:
+                    | "none"
+                    | "bloxproducts"
+                    | "robloxcheatz"
+                    | "giveaway"
+                    | "replacement"
+                ) => {
+                  if (selected === "none") {
+                    setOrderID(null);
+                    return;
+                  }
 
-                            const blob = new Blob([serial], { type: "text/plain" });
-                            const url = URL.createObjectURL(blob);
-                            const a = document.createElement("a");
-                            a.href = url;
-                            a.download = "serials.txt";
-                            a.click();
-                            URL.revokeObjectURL(url);
-                            toast.success("Serial key downloaded!");
-                        }} className="mb-2" variant={"secondary"}>Download Serials <DownloadIcon/></Button>
-                        <AlertDialogCancel onClick={() => {
-                            setIsOpen(false);
-                            setNewKey(null);
-                            setOrderID(null);
-                        }}>Dismiss</AlertDialogCancel>
-                    </>
-                ) : (
-                    <>
-                        <AlertDialogCancel onClick={() => {
-                            setIsOpen(false);
-                            setNewKey(null);
-                            setOrderID(null);
-                        }}>Cancel</AlertDialogCancel>
-                        <AlertDialogAction onClick={() => {
-                            if (amount < 1) {
-                                return toast.error("Amount must be greater than 0.");
-                            }
+                  if (selected === "giveaway") {
+                    setOrderID(`Giveaway`);
+                  }
 
-                            if (amount > 50) {
-                                return toast.error("Amount must be less than 50.");
-                            }
+                  if (selected === "bloxproducts") {
+                    setOrderID(`Bloxproducts - {UUID}`);
+                  }
 
-                            toast.promise(GenerateSerial(order_id, amount), {
-                                loading: "Generating serial key...",
-                                success: (serial) => {
-                                    if ((serial as error_props).error) {
-                                        throw new Error("Failed to generate serial key: " + (serial as error_props).error);
-                                    }
+                  if (selected === "robloxcheatz") {
+                    setOrderID(`RobloxCheatz - {UUID}`);
+                  }
 
-                                    setNewKey(serial as string[]);
-                                    return "Serial key generated!";
-                                },
-                                error: "Failed to generate serial key."
-                            })
-                        }}>Continue</AlertDialogAction>
-                    </>
-                )}
-            </AlertDialogFooter>
-            </AlertDialogContent>
-        </AlertDialog>
-    )
+                  if (selected === "replacement") {
+                    setOrderID(`REPLACEMENT`);
+                  }
+                }}
+              >
+                <SelectTrigger className="w-[180px] mr-auto">
+                  <SelectValue placeholder="Template" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="none">No Template</SelectItem>
+                  <SelectItem value="replacement">Replacement</SelectItem>
+
+                  <SelectGroup>
+                    <SelectLabel>Reseller</SelectLabel>
+                    <SelectItem value="bloxproducts">Bloxproducts</SelectItem>
+                    <SelectItem value="robloxcheatz">Robloxcheatz</SelectItem>
+                  </SelectGroup>
+
+                  <SelectGroup>
+                    <SelectLabel>Misc</SelectLabel>
+                    <SelectItem value="giveaway">Discord Giveaway</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
+              </Select>
+
+              <AlertDialogCancel
+                onClick={() => {
+                  setIsOpen(false);
+                  setNewKey(null);
+                  setOrderID(null);
+                }}
+              >
+                Cancel
+              </AlertDialogCancel>
+              <AlertDialogAction
+                onClick={() => {
+                  if (amount < 1) {
+                    return toast.error("Amount must be greater than 0.");
+                  }
+
+                  if (amount > 50) {
+                    return toast.error("Amount must be less than 50.");
+                  }
+
+                  toast.promise(GenerateSerial(order_id, amount), {
+                    loading: "Generating serial key...",
+                    success: (serial) => {
+                      if ((serial as error_props).error) {
+                        throw new Error(
+                          "Failed to generate serial key: " +
+                            (serial as error_props).error
+                        );
+                      }
+
+                      setNewKey(serial as string[]);
+                      return "Serial key generated!";
+                    },
+                    error: "Failed to generate serial key.",
+                  });
+                }}
+              >
+                Continue
+              </AlertDialogAction>
+            </>
+          )}
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 }
--- src/server/redeemkey.ts
+++ src/server/redeemkey.ts
@@ -4,219 +4,251 @@ import { sql } from "@vercel/postgres";
 import { isUserAllowedOnDashboard } from "./authutils";
 
 export async function RedeemKey(serial: string, user_id: string) {
-    if (user_id === "skibidiSigma") {
-        return {
-            status: 403,
-            error: "user_id is invalid"
-        }
-    }
-
-    const { rows } = await sql`SELECT * FROM mspaint_keys WHERE serial = ${serial}`;
-
-    if (rows.length === 0 || rows[0].claimed === true) {
-        return {
-            status: 404,
-            error: "key not found"
-        }
-    }
-
-    const LRM_Headers = {
-        "Authorization": `Bearer ${process.env.LRM_PROXY_API_KEY}`,
-        "Content-Type": "application/json"
-    }
-
-    const checkpointKeyResponse = await fetch(`${process.env.LRM_PROXY_URL}/v3/projects/${process.env.LRM_PROJECT_ID}/users?discord_id=${user_id}`, {
-        method: "GET",
-        headers: LRM_Headers
-    })
-
-    if (checkpointKeyResponse.status !== 200) {
-        return {
-            status: 500,
-            error: "internal luarmor api error, returned status code " + checkpointKeyResponse.status + ", please wait a few minutes and try again"
-        }
-    }
-
-    const checkpointKey = await checkpointKeyResponse.json();
-    let does_user_have_checkpoint_key = false;
-    if (checkpointKey.users.length !== 0) {
-        if (checkpointKey.users[0].note === "Ad Reward") {
-            does_user_have_checkpoint_key = true;
-        }
-
-        if (!does_user_have_checkpoint_key) {
-            return {
-                status: 403,
-                error: "user already has a permanent key"
-            }
-        }
-    }
-
-    if (does_user_have_checkpoint_key) {
-        await fetch(`${process.env.LRM_PROXY_URL}/v3/projects/${process.env.LRM_PROJECT_ID}/users?user_key=${checkpointKey.users[0].user_key}`, {
-            method: "DELETE",
-            headers: LRM_Headers
-        })
-    }
-
-    const keyCreationResponse = await fetch(`${process.env.LRM_PROXY_URL}/v3/projects/${process.env.LRM_PROJECT_ID}/users`, {
-        method: "POST",
-        headers: LRM_Headers,
-        body: JSON.stringify({
-            discord_id: user_id,
-            note: (rows[0].order_id ?? "Lifetime key sellapp") + " - " + serial,
-        })
-    })
-
-    const keyCreation = await keyCreationResponse.json();
-
-    if (keyCreation.error) {
-        return {
-            status: 500,
-            error: keyCreation.message
-        }
-    }
-
-    await sql`UPDATE mspaint_keys SET claimed = true, claimed_discord_id = ${user_id}, lrm_serial = ${keyCreation.user_key} WHERE serial = ${serial}`;
-    
-    if ((rows[0].order_id as string).toLowerCase().includes("bloxproducts")) {
-        await fetch(`${process.env.BLOXPRODUCTS_WEBHOOK}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                "content": null,
-                "embeds": [
-                  {
-                    "title": "BLOXPRODUCTS - mspaint key purchased",
-                    "description": "a key was purchased & redeemed via the bloxproducts.",
-                    "color": 5814783,
-                    "fields": [
-                      {
-                        "name": "mspaint serial",
-                        "value": `||${serial}||`,
-                        "inline": true
-                      },
-                      {
-                        "name": "Order ID",
-                        "value": `||${rows[0].order_id}||`,
-                        "inline": true
-                      },
-                      {
-                        "name": "Discord",
-                        "value": `<@${user_id}> (${user_id})`,
-                        "inline": true
-                      },
-                      {
-                        "name": "Luarmor Serial",
-                        "value":`||${keyCreation.user_key}||`,
-                        "inline": true
-                      }
-                    ]
-                  }
-                ],
-                "attachments": []
-            })
-        })
-    }
-    
+  if (user_id === "skibidiSigma") {
     return {
-        status: 200,
-        success: "key redeemed successfully",
-        user_key: keyCreation.user_key
+      status: 403,
+      error: "user_id is invalid",
     };
+  }
+
+  const { rows } =
+    await sql`SELECT * FROM mspaint_keys WHERE serial = ${serial}`;
+
+  if (rows.length === 0 || rows[0].claimed === true) {
+    return {
+      status: 404,
+      error: "key not found",
+    };
+  }
+
+  const LRM_Headers = {
+    Authorization: `Bearer ${process.env.LRM_PROXY_API_KEY}`,
+    "Content-Type": "application/json",
+  };
+
+  const checkpointKeyResponse = await fetch(
+    `${process.env.LRM_PROXY_URL}/v3/projects/${process.env.LRM_PROJECT_ID}/users?discord_id=${user_id}`,
+    {
+      method: "GET",
+      headers: LRM_Headers,
+    }
+  );
+
+  if (checkpointKeyResponse.status !== 200) {
+    return {
+      status: 500,
+      error:
+        "internal luarmor api error, returned status code " +
+        checkpointKeyResponse.status +
+        ", please wait a few minutes and try again",
+    };
+  }
+
+  const checkpointKey = await checkpointKeyResponse.json();
+  let does_user_have_checkpoint_key = false;
+  if (checkpointKey.users.length !== 0) {
+    if (checkpointKey.users[0].note === "Ad Reward") {
+      does_user_have_checkpoint_key = true;
+    }
+
+    if (!does_user_have_checkpoint_key) {
+      return {
+        status: 403,
+        error: "user already has a permanent key",
+      };
+    }
+  }
+
+  if (does_user_have_checkpoint_key) {
+    await fetch(
+      `${process.env.LRM_PROXY_URL}/v3/projects/${process.env.LRM_PROJECT_ID}/users?user_key=${checkpointKey.users[0].user_key}`,
+      {
+        method: "DELETE",
+        headers: LRM_Headers,
+      }
+    );
+  }
+
+  const keyCreationResponse = await fetch(
+    `${process.env.LRM_PROXY_URL}/v3/projects/${process.env.LRM_PROJECT_ID}/users`,
+    {
+      method: "POST",
+      headers: LRM_Headers,
+      body: JSON.stringify({
+        discord_id: user_id,
+        note: (rows[0].order_id ?? "Lifetime key sellapp") + " - " + serial,
+      }),
+    }
+  );
+
+  const keyCreation = await keyCreationResponse.json();
+
+  if (keyCreation.error) {
+    return {
+      status: 500,
+      error: keyCreation.message,
+    };
+  }
+
+  await sql`UPDATE mspaint_keys SET claimed = true, claimed_discord_id = ${user_id}, lrm_serial = ${keyCreation.user_key} WHERE serial = ${serial}`;
+
+  if ((rows[0].order_id as string).toLowerCase().includes("bloxproducts")) {
+    await fetch(`${process.env.BLOXPRODUCTS_WEBHOOK}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        content: null,
+        embeds: [
+          {
+            title: "BLOXPRODUCTS - mspaint key purchased",
+            description: "a key was purchased & redeemed via the bloxproducts.",
+            color: 5814783,
+            fields: [
+              {
+                name: "mspaint serial",
+                value: `||${serial}||`,
+                inline: true,
+              },
+              {
+                name: "Order ID",
+                value: `||${rows[0].order_id}||`,
+                inline: true,
+              },
+              {
+                name: "Discord",
+                value: `<@${user_id}> (${user_id})`,
+                inline: true,
+              },
+              {
+                name: "Luarmor Serial",
+                value: `||${keyCreation.user_key}||`,
+                inline: true,
+              },
+            ],
+          },
+        ],
+        attachments: [],
+      }),
+    });
+  }
+
+  return {
+    status: 200,
+    success: "key redeemed successfully",
+    user_key: keyCreation.user_key,
+  };
 }
 
 function _internal_create_serial() {
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let serial = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let serial = "";
 
-    for (let i = 0; i < 16; i++) {
-        serial += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
+  for (let i = 0; i < 16; i++) {
+    serial += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
 
-    return serial;
+  return serial;
+}
+
+function generate_uuid() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0,
+      v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
 export async function GenerateSerial(invoiceID: string | null, amount: number) {
-    const allowed = await isUserAllowedOnDashboard();
+  const allowed = await isUserAllowedOnDashboard();
 
-    if (!allowed) {
-        return {
-            status: 403,
-            error: "nah"
-        }
+  if (!allowed) {
+    return {
+      status: 403,
+      error: "nah",
+    };
+  }
+
+  let actualInvoiceId = invoiceID;
+
+  if (typeof invoiceID === "string") {
+    if (actualInvoiceId?.trim() === "") {
+      actualInvoiceId = "REPLACEMENT";
     }
+  } else if (invoiceID === null) {
+    actualInvoiceId = "REPLACEMENT";
+  } else if (invoiceID === undefined) {
+    actualInvoiceId = "REPLACEMENT";
+  }
 
-    let actualInvoiceId = invoiceID;
+  if (amount < 1) {
+    return {
+      status: 400,
+      error: "amount must be greater than 0",
+    };
+  }
 
-    if (typeof invoiceID === "string") {
-        if (actualInvoiceId?.trim() === "") { actualInvoiceId = "REPLACEMENT" }
-    } else if (invoiceID === null) {
-        actualInvoiceId = "REPLACEMENT";
-    }
+  if (amount > 50) {
+    return {
+      status: 400,
+      error: "amount must be less than 50",
+    };
+  }
 
-    if (amount < 1) {
-        return {
-            status: 400,
-            error: "amount must be greater than 0"
-        }
-    }
+  const serials = [];
+  for (let i = 0; i < amount; i++) {
+    const serial = _internal_create_serial();
 
-    if (amount > 50) {
-        return {
-            status: 400,
-            error: "amount must be less than 50"
-        }
-    }
+    const invoiceIDToInsert = actualInvoiceId?.replaceAll(
+      "{UUID}",
+      generate_uuid()
+    );
 
-    const serials = [];
-    for (let i = 0; i < amount; i++) {
-        const serial = _internal_create_serial();
-        await sql`INSERT INTO mspaint_keys (serial, order_id, claimed) VALUES (${serial}, ${actualInvoiceId}, false);`;
-        serials.push(serial);
-    }
+    await sql`INSERT INTO mspaint_keys (serial, order_id, claimed) VALUES (${serial}, ${invoiceIDToInsert}, false);`;
+    serials.push(serial);
+  }
 
-    return serials;
+  return serials;
 }
 
 export async function DeleteSerial(serial: string) {
-    const allowed = await isUserAllowedOnDashboard();
+  const allowed = await isUserAllowedOnDashboard();
 
-    if (!allowed) {
-        return {
-            status: 403,
-            error: "nah"
-        }
-    }
-
-    const { rows } = await sql`SELECT * FROM mspaint_keys WHERE serial = ${serial}`;
-
-    if (rows.length === 0) {
-        return {
-            status: 404,
-            error: "key not found"
-        }
-    }
-
-    await sql`DELETE FROM mspaint_keys WHERE serial = ${serial}`;
+  if (!allowed) {
     return {
-        status: 200,
-        success: "key deleted successfully"
-    }
+      status: 403,
+      error: "nah",
+    };
+  }
+
+  const { rows } =
+    await sql`SELECT * FROM mspaint_keys WHERE serial = ${serial}`;
+
+  if (rows.length === 0) {
+    return {
+      status: 404,
+      error: "key not found",
+    };
+  }
+
+  await sql`DELETE FROM mspaint_keys WHERE serial = ${serial}`;
+  return {
+    status: 200,
+    success: "key deleted successfully",
+  };
 }
 
 export async function GetAllSerialData() {
-    const allowed = await isUserAllowedOnDashboard();
+  const allowed = await isUserAllowedOnDashboard();
 
-    if (!allowed) {
-        return {
-            status: 403,
-            error: "nah"
-        }
-    }
+  if (!allowed) {
+    return {
+      status: 403,
+      error: "nah",
+    };
+  }
 
-    const { rows } = await sql`SELECT * FROM mspaint_keys`;
-    return rows;
+  const { rows } = await sql`SELECT * FROM mspaint_keys`;
+  return rows;
 }
