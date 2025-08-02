4f924680381a68899eb495d138d935e46744d4ed
upio <75510171+notpoiu@users.noreply.github.com>
2025-08-02 17:24:05 -0400

    fix: fixed wording thx bye

--- src/app/purchase/completed/components/redeem.tsx
+++ src/app/purchase/completed/components/redeem.tsx
@@ -1,15 +1,15 @@
 "use client";
 
 import {
-    AlertDialog,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-} from "@/components/ui/alert-dialog"
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 import { Button } from "@/components/ui/button";
 import { RedeemKey } from "@/server/redeemkey";
@@ -17,7 +17,7 @@ import { toast } from "sonner";
 import { RainbowButton } from "@/components/magicui/rainbow-button";
 import { Confetti } from "@/components/magicui/confetti";
 import { useRouter } from "next/navigation";
-import { useRef, useEffect, useState, useCallback, useTransition } from 'react'
+import { useRef, useEffect, useState, useCallback, useTransition } from "react";
 
 /**
  * Wrapper around `router.refresh()` from `next/navigation` `useRouter()` to return Promise, and resolve after refresh completed
@@ -25,97 +25,123 @@ import { useRef, useEffect, useState, useCallback, useTransition } from 'react'
  * @returns Refresh function
  */
 function useRouterRefresh() {
-  const router = useRouter()
-  const [isPending, startTransition] = useTransition()
-  const resolveRef = useRef<((value?: void) => void) | null>(null)
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const resolveRef = useRef<((value?: void) => void) | null>(null);
 
   useEffect(() => {
     if (!isPending && resolveRef.current) {
-      resolveRef.current(undefined)
-      resolveRef.current = null
+      resolveRef.current(undefined);
+      resolveRef.current = null;
     }
-  }, [isPending])
+  }, [isPending]);
 
   return useCallback(() => {
-    return new Promise<void>(res => {
-      resolveRef.current = res
+    return new Promise<void>((res) => {
+      resolveRef.current = res;
       startTransition(() => {
-        router.refresh()
-      })
-    })
-  }, [router, startTransition])
+        router.refresh();
+      });
+    });
+  }, [router, startTransition]);
 }
 
-export function RedeemComponent({serial, username, user_id}: {
-    serial: null | string;
-    username: string;
-    user_id: string;
+export function RedeemComponent({
+  serial,
+  username,
+  user_id,
+}: {
+  serial: null | string;
+  username: string;
+  user_id: string;
 }) {
-    const [open, setOpen] = useState(false);
-    const [keyClaimed, setKeyClaimed] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [keyClaimed, setKeyClaimed] = useState(false);
 
-    const refresh = useRouterRefresh()
+  const refresh = useRouterRefresh();
 
-    return (
-        <>
-            {keyClaimed && (
-                <div className="fixed inset-0 pointer-events-none" style={{ zIndex: 9999 }}>
-                    <Confetti 
-                        options={{
-                            spread: 360, 
-                            origin: {x: 0.5, y: 0.5}, 
-                            decay: 0.9, 
-                            gravity: 0.5, 
-                            startVelocity: 45, 
-                            particleCount: 80
-                        }} 
-                        className="w-full h-full" 
-                        style={{ pointerEvents: 'none' }}
-                    />
-                </div>
-            )}
-            <AlertDialog open={open} onOpenChange={setOpen}>
-                <AlertDialogTrigger asChild>
-                    <RainbowButton variant={"default"} size={"lg"} disabled={keyClaimed || serial == null} style={{fontWeight: 'bold'}}>
-                        {(keyClaimed || serial == null) ? "Successfully claimed!" : `Redeem mspaint for ${username}`}
-                    </RainbowButton>
-                </AlertDialogTrigger>
-                <AlertDialogContent className="bg-black">
-                    <AlertDialogHeader>
-                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-                        <AlertDialogDescription>
-                            This action cannot be undone. This will redeem the mspaint key for {username}. We are not responsible for any lost keys.
-                        </AlertDialogDescription>
-                    </AlertDialogHeader>
+  return (
+    <>
+      {keyClaimed && (
+        <div
+          className="fixed inset-0 pointer-events-none"
+          style={{ zIndex: 9999 }}
+        >
+          <Confetti
+            options={{
+              spread: 360,
+              origin: { x: 0.5, y: 0.5 },
+              decay: 0.9,
+              gravity: 0.5,
+              startVelocity: 45,
+              particleCount: 80,
+            }}
+            className="w-full h-full"
+            style={{ pointerEvents: "none" }}
+          />
+        </div>
+      )}
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogTrigger asChild>
+          <RainbowButton
+            variant={"default"}
+            size={"lg"}
+            disabled={keyClaimed || serial == null}
+            style={{ fontWeight: "bold" }}
+          >
+            {keyClaimed || serial == null
+              ? "Successfully claimed!"
+              : `Redeem mspaint for ${username}`}
+          </RainbowButton>
+        </AlertDialogTrigger>
+        <AlertDialogContent className="bg-black">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will redeem the mspaint key for{" "}
+              {username}. We are not responsible for any lost keys.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
 
-                    <AlertDialogFooter>
-                        <AlertDialogCancel>Cancel</AlertDialogCancel>
-                        <Button onClick={() => {
-                            toast.promise(async () => {
-                                const response = await RedeemKey(serial as string, user_id);
-                                if (response.status !== 200) {
-                                    throw new Error(response.error);
-                                }
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <Button
+              onClick={() => {
+                toast.promise(
+                  async () => {
+                    const response = await RedeemKey(serial as string, user_id);
+                    if (response.status !== 200) {
+                      throw new Error(response.error);
+                    }
 
-                                return response;
-                            }, {
-                                loading: "Redeeming lifetime mspaint key...",
-                                success: () => {
-                                    setOpen(false);
-                                    refresh().then(() => {
-                                        setKeyClaimed(true);
-                                    })
-                                    return "Key redeemed successfully for " + username + "! You can now access mspaint via #getscript in discord.";
-                                },
-                                error: (error_data) => {
-                                    setOpen(false);
-                                    return "Failed to redeem key: " + error_data.message;
-                                },
-                            });
-                        }}>Continue</Button>
-                    </AlertDialogFooter>
-                </AlertDialogContent>
-            </AlertDialog>
-        </>
-    );
+                    return response;
+                  },
+                  {
+                    loading: "Redeeming mspaint key...",
+                    success: () => {
+                      setOpen(false);
+                      refresh().then(() => {
+                        setKeyClaimed(true);
+                      });
+                      return (
+                        "Key redeemed successfully for " +
+                        username +
+                        "! You can now access mspaint via #getscript in discord."
+                      );
+                    },
+                    error: (error_data) => {
+                      setOpen(false);
+                      return "Failed to redeem key: " + error_data.message;
+                    },
+                  }
+                );
+              }}
+            >
+              Continue
+            </Button>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  );
 }
