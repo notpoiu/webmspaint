2a5a8a9b5114ec3f6960a7ebec8b12296142e8d3
Blukery <Blukery@users.noreply.github.com>
2025-08-08 09:16:07 -0300

    feat: refresh sub. dashboard on hwid reset

--- src/components/mini-dashboard-card.tsx
+++ src/components/mini-dashboard-card.tsx
@@ -284,6 +284,7 @@ export default function MiniDashboardCard({
                                       }
                                     }
                                   );
+                                  router.refresh();
                                 }
                               }
                               >
