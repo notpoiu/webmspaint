c47fc5ea7fcb9c68247de33c52a72094d6cf25f5
upio <75510171+notpoiu@users.noreply.github.com>
2025-08-28 11:49:40 -0400

    types: add types for addons and prepare for addons

--- src/components/obsidian/DynamicTab.tsx
+++ src/components/obsidian/DynamicTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from "react";
+import React from "react";
 
 import { Groupbox } from "./elements/GroupBox";
 import { TabContainer, TabLeft, TabRight } from "./elements/Tab";
@@ -15,156 +15,157 @@ import Slider from "./elements/Slider";
 
 // Parsers //
 export const ElementParser: React.FC<{
-	element: UIElement;
-	stateKeyPrefix?: string;
+  element: UIElement;
+  stateKeyPrefix?: string;
 }> = ({ element, stateKeyPrefix }) => {
-	if ("visible" in element && !element.visible) return null;
+  if ("visible" in element && !element.visible) return null;
 
-	switch (element.type) {
-		case "Toggle":
-			return (
-				<Toggle
-					text={element.text}
-					checked={element.value}
-					stateKey={`${stateKeyPrefix || "global"}:el:Toggle:${element.index}`}
-				/>
-			);
+  switch (element.type) {
+    case "Toggle":
+      return (
+        <Toggle
+          text={element.text}
+          checked={element.value}
+          stateKey={`${stateKeyPrefix || "global"}:el:Toggle:${element.index}`}
+        />
+      );
 
-		case "Label":
-			return <Label>{element.text}</Label>;
+    case "Label":
+      return <Label>{element.text}</Label>;
 
-		case "Button":
-			return (
-				<Button
-					text={element.text}
-					subButton={element.subButton && element.subButton}
-				/>
-			);
+    case "Button":
+      return (
+        <Button
+          text={element.text}
+          subButton={element.subButton && element.subButton}
+        />
+      );
 
-		case "Dropdown":
-			return (
-				<Dropdown
-					text={element.text}
-					value={element.value}
-					options={element.properties.values}
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					multi={(element as any).multi ?? (element as any).properties?.multi}
-					// eslint-disable-next-line @typescript-eslint/no-explicit-any
-					disabledValues={(element as any).properties?.disabledValues || []}
-					stateKey={`${stateKeyPrefix || "global"}:el:Dropdown:${element.index
-						}`}
-				/>
-			);
+    case "Dropdown":
+      return (
+        <Dropdown
+          text={element.text}
+          value={element.value}
+          options={element.properties.values}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          multi={(element as any).multi ?? (element as any).properties?.multi}
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          disabledValues={(element as any).properties?.disabledValues || []}
+          stateKey={`${stateKeyPrefix || "global"}:el:Dropdown:${
+            element.index
+          }`}
+        />
+      );
 
-		case "Slider":
-			return (
-				<Slider
-					text={element.text}
-					value={element.value}
-					min={element.properties.min}
-					max={element.properties.max}
-					compact={element.properties.compact}
-					rounding={element.properties.rounding}
-					prefix={element.properties.prefix}
-					suffix={element.properties.suffix}
-					stateKey={`${stateKeyPrefix || "global"}:el:Slider:${element.index}`}
-				/>
-			);
+    case "Slider":
+      return (
+        <Slider
+          text={element.text}
+          value={element.value}
+          min={element.properties.min}
+          max={element.properties.max}
+          compact={element.properties.compact}
+          rounding={element.properties.rounding}
+          prefix={element.properties.prefix}
+          suffix={element.properties.suffix}
+          stateKey={`${stateKeyPrefix || "global"}:el:Slider:${element.index}`}
+        />
+      );
 
-		case "Input":
-			return (
-				<Input
-					text={element.text}
-					value={element.value}
-					placeholder={element.properties.placeholder}
-					stateKey={`${stateKeyPrefix || "global"}:el:Input:${element.index}`}
-				/>
-			);
+    case "Input":
+      return (
+        <Input
+          text={element.text}
+          value={element.value}
+          placeholder={element.properties.placeholder}
+          stateKey={`${stateKeyPrefix || "global"}:el:Input:${element.index}`}
+        />
+      );
 
-		case "Divider":
-			return <Divider />;
+    case "Divider":
+      return <Divider />;
 
-		case "Image":
-			return (
-				<ObsidianImage
-					image={element.properties.image}
-					transparency={element.properties.transparency}
-					scaleType={element.properties.scaleType}
-					color={element.properties.color}
-					rectOffset={element.properties.rectOffset}
-					height={element.properties.height}
-					rectSize={element.properties.rectSize}
-				/>
-			);
+    case "Image":
+      return (
+        <ObsidianImage
+          image={element.properties.image}
+          transparency={element.properties.transparency}
+          scaleType={element.properties.scaleType}
+          color={element.properties.color}
+          rectOffset={element.properties.rectOffset}
+          height={element.properties.height}
+          rectSize={element.properties.rectSize}
+        />
+      );
 
-		default:
-			// const exhaustiveCheck: never = element;
-			return (
-				<div className="text-red-400 text-left">
-					Unknown element type:{" "}
-					{(element as { type: string }).type || "Unknown"}
-				</div>
-			);
-	}
+    default:
+      // const exhaustiveCheck: never = element;
+      return (
+        <div className="text-red-400 text-left">
+          Unknown element type:{" "}
+          {(element as { type: string }).type || "Unknown"}
+        </div>
+      );
+  }
 };
 
 export const TabParser: React.FC<{ tabData: TabData | null }> = ({
-	tabData,
+  tabData,
 }) => {
-	if (!tabData) return null;
+  if (!tabData) return null;
 
-	const { groupboxes, tabboxes } = tabData;
-	return (
-		<TabContainer>
-			<TabLeft>
-				{tabboxes?.Left &&
-					Object.values(tabboxes.Left).map((tabbox) => (
-						<Tabbox
-							key={tabbox.name}
-							tabs={tabbox.tabs}
-							scope={`tab:${tabData.name}:left:tabbox:${tabbox.name}`}
-						/>
-					))}
-				{groupboxes?.Left &&
-					Object.values(groupboxes.Left)
-						.sort((a, b) => a.order - b.order)
-						.map((gb) => (
-						<Groupbox key={gb.name} title={gb.name}>
-							{gb.elements.map((el) => (
-								<ElementParser
-									key={`left-gb-${gb.name}-${el.index}`}
-									element={el}
-									stateKeyPrefix={`tab:${tabData.name}:left:groupbox:${gb.name}`}
-								/>
-							))}
-						</Groupbox>
-					))}
-			</TabLeft>
+  const { groupboxes, tabboxes } = tabData;
+  return (
+    <TabContainer>
+      <TabLeft>
+        {tabboxes?.Left &&
+          Object.values(tabboxes.Left).map((tabbox) => (
+            <Tabbox
+              key={tabbox.name}
+              tabs={tabbox.tabs}
+              scope={`tab:${tabData.name}:left:tabbox:${tabbox.name}`}
+            />
+          ))}
+        {groupboxes?.Left &&
+          Object.values(groupboxes.Left)
+            .sort((a, b) => a.order - b.order)
+            .map((gb) => (
+              <Groupbox key={gb.name} title={gb.name}>
+                {gb.elements.map((el) => (
+                  <ElementParser
+                    key={`left-gb-${gb.name}-${el.index}`}
+                    element={el}
+                    stateKeyPrefix={`tab:${tabData.name}:left:groupbox:${gb.name}`}
+                  />
+                ))}
+              </Groupbox>
+            ))}
+      </TabLeft>
 
-			<TabRight>
-				{tabboxes?.Right &&
-					Object.values(tabboxes.Right).map((tabbox) => (
-						<Tabbox
-							key={tabbox.name}
-							tabs={tabbox.tabs}
-							scope={`tab:${tabData.name}:right:tabbox:${tabbox.name}`}
-						/>
-					))}
-				{groupboxes?.Right &&
-					Object.values(groupboxes.Right)
-						.sort((a, b) => a.order - b.order)
-						.map((gb) => (
-						<Groupbox key={gb.name} title={gb.name}>
-							{gb.elements.map((el) => (
-								<ElementParser
-									key={`right-gb-${gb.name}-${el.index}`}
-									element={el}
-									stateKeyPrefix={`tab:${tabData.name}:right:groupbox:${gb.name}`}
-								/>
-							))}
-						</Groupbox>
-					))}
-			</TabRight>
-		</TabContainer>
-	);
+      <TabRight>
+        {tabboxes?.Right &&
+          Object.values(tabboxes.Right).map((tabbox) => (
+            <Tabbox
+              key={tabbox.name}
+              tabs={tabbox.tabs}
+              scope={`tab:${tabData.name}:right:tabbox:${tabbox.name}`}
+            />
+          ))}
+        {groupboxes?.Right &&
+          Object.values(groupboxes.Right)
+            .sort((a, b) => a.order - b.order)
+            .map((gb) => (
+              <Groupbox key={gb.name} title={gb.name}>
+                {gb.elements.map((el) => (
+                  <ElementParser
+                    key={`right-gb-${gb.name}-${el.index}`}
+                    element={el}
+                    stateKeyPrefix={`tab:${tabData.name}:right:groupbox:${gb.name}`}
+                  />
+                ))}
+              </Groupbox>
+            ))}
+      </TabRight>
+    </TabContainer>
+  );
 };
--- src/components/obsidian/element.types.ts
+++ src/components/obsidian/element.types.ts
@@ -1,163 +1,165 @@
+// Data Structures //
+interface Color3 {
+  r: number;
+  g: number;
+  b: number;
+}
+
+interface Vector2 {
+  x: number;
+  y: number;
+}
+
 // Element Types //
 interface BaseElement {
-	index: number;
-	visible: boolean;
-	type: string;
-	text: string;
-	disabled: boolean;
+  index: number;
+  visible: boolean;
+  type: string;
+  text: string;
+  disabled: boolean;
+}
+
+export interface KeybindElement extends BaseElement {
+  type: "KeyPicker";
+  mode: "Toggle" | "Hold" | "Always";
+  value: string;
+}
+
+export interface ColorPickerElement extends BaseElement {
+  type: "ColorPicker";
+  value: Color3;
 }
 
 export interface ToggleElement extends BaseElement {
-	type: "Toggle";
-	value: boolean;
+  type: "Toggle";
+  value: boolean;
 }
 
 export interface LabelElement extends BaseElement {
-	type: "Label";
-	properties: {
-		doesWrap: boolean;
-	};
+  type: "Label";
+  properties: {
+    doesWrap: boolean;
+  };
 }
 
 export interface ButtonElement extends BaseElement {
-	type: "Button";
-	subButton?: {
-		text: string;
-	};
+  type: "Button";
+  subButton?: {
+    text: string;
+  };
 }
 
 export interface DropdownElement extends BaseElement {
-	type: "Dropdown";
-	value: string | { [key: string]: boolean };
-	multi: boolean | undefined;
-	properties: {
-		values: string[];
-		disabledValues: string[];
-	};
+  type: "Dropdown";
+  value: string | { [key: string]: boolean };
+  multi: boolean | undefined;
+  properties: {
+    values: string[];
+    disabledValues: string[];
+  };
 }
 
 export interface SliderElement extends BaseElement {
-	type: "Slider";
-	value: number;
-	properties: {
-		min: number;
-		max: number;
-		compact: boolean | undefined;
-		rounding: number | undefined;
-		prefix: string;
-		suffix: string;
-	};
+  type: "Slider";
+  value: number;
+  properties: {
+    min: number;
+    max: number;
+    compact: boolean | undefined;
+    rounding: number | undefined;
+    prefix: string;
+    suffix: string;
+  };
 }
 
 export interface InputElement extends BaseElement {
-	type: "Input";
-	value: string;
-	properties: {
-		placeholder: string;
-		finished: boolean;
-		emptyReset: string;
-		numeric: boolean;
-		clearTextOnFocus: boolean;
-		allowEmpty: boolean;
-	};
+  type: "Input";
+  value: string;
+  properties: {
+    placeholder: string;
+    finished: boolean;
+    emptyReset: string;
+    numeric: boolean;
+    clearTextOnFocus: boolean;
+    allowEmpty: boolean;
+  };
 }
 
 export interface DividerElement {
-	index: number;
-	type: "Divider";
-	properties: object;
-	visible?: boolean;
+  index: number;
+  type: "Divider";
+  properties: object;
+  visible?: boolean;
 }
 
-/*
-info.properties = {
-			image = element.Image,
-			color = UIExtractor:Serialize(element.Color),
-			rectOffset = UIExtractor:Serialize(element.RectOffset),
-			rectSize = UIExtractor:Serialize(element.RectSize),
-			height = UIExtractor:Serialize(element.Height),
-			scaleType = element.ScaleType.Name,
-			transparency = element.Transparency
-		}
-*/
 export interface ImageElement {
-	index: number;
-	type: "Image";
-	properties: {
-		image: string;
-		color: {
-			r: number;
-			g: number;
-			b: number;
-		};
-		rectOffset: {
-			x: number;
-			y: number;
-		};
-		rectSize: {
-			x: number;
-			y: number;
-		};
-		height: number;
-		scaleType: string;
-		transparency: number;
-	};
-	visible?: boolean;
+  index: number;
+  type: "Image";
+  properties: {
+    image: string;
+    color: Color3;
+    rectOffset: Vector2;
+    rectSize: Vector2;
+    height: number;
+    scaleType: string;
+    transparency: number;
+  };
+  visible?: boolean;
 }
 
 export type UIElement =
-	| ToggleElement
-	| LabelElement
-	| ButtonElement
-	| DropdownElement
-	| SliderElement
-	| InputElement
-	| DividerElement
-	| ImageElement;
+  | ToggleElement
+  | LabelElement
+  | ButtonElement
+  | DropdownElement
+  | SliderElement
+  | InputElement
+  | DividerElement
+  | ImageElement;
 
 // JSON File Types //
 export interface GroupboxData {
-	name: string;
-	order: number;
-	side: "Left" | "Right" | "Unknown";
-	elements: UIElement[];
+  name: string;
+  order: number;
+  side: "Left" | "Right" | "Unknown";
+  elements: UIElement[];
 }
 
 export interface TabboxTab {
-	type: "Tab";
-	name: string;
-	order: number;
-	elements: UIElement[];
+  type: "Tab";
+  name: string;
+  order: number;
+  elements: UIElement[];
 }
 
 export interface TabboxData {
-	type: "Tabbox";
-	name: number;
-	side: "Left" | "Right" | "Unknown";
-	tabs: {
-		[key: string]: TabboxTab;
-	};
+  type: "Tabbox";
+  name: number;
+  side: "Left" | "Right" | "Unknown";
+  tabs: {
+    [key: string]: TabboxTab;
+  };
 }
 
 export interface TabData {
-	name: string;
-	type: string;
-	icon: string;
-	order: number;
-	tabboxes: {
-		Left: TabboxData[];
-		Right: TabboxData[];
-		Unknown: TabboxData[];
-	};
-	groupboxes: {
-		Left: { [key: string]: GroupboxData };
-		Right: { [key: string]: GroupboxData };
-		Unknown: { [key: string]: GroupboxData };
-	};
+  name: string;
+  type: string;
+  icon: string;
+  order: number;
+  tabboxes: {
+    Left: TabboxData[];
+    Right: TabboxData[];
+    Unknown: TabboxData[];
+  };
+  groupboxes: {
+    Left: { [key: string]: GroupboxData };
+    Right: { [key: string]: GroupboxData };
+    Unknown: { [key: string]: GroupboxData };
+  };
 }
 
 export interface UIData {
-	tabs: {
-		[key: string]: TabData;
-	};
+  tabs: {
+    [key: string]: TabData;
+  };
 }
