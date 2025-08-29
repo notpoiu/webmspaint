616e85de5690d4cf6456b1c0838fba46f6b9ed99
upio <75510171+notpoiu@users.noreply.github.com>
2025-08-29 16:45:05 -0400

    fix: made it so the scrolling states are independent depending on which side (left or right)

--- src/components/obsidian/elements/Tab.tsx
+++ src/components/obsidian/elements/Tab.tsx
@@ -2,16 +2,24 @@ import type { ReactNode } from "react";
 
 export function TabContainer({ children }: { children: ReactNode }) {
     return (
-        <div className="h-full overflow-y-auto no-scrollbar flex flex-col">
-            <div className="flex flex-row w-full">{children}</div>
+        <div className="h-full flex flex-col">
+            <div className="flex flex-row w-full h-full overflow-hidden min-h-0">{children}</div>
         </div>
     );
 }
 
 export function TabLeft({ children }: { children: ReactNode }) {
-    return <div className="w-full pr-1">{children}</div>;
+    return (
+        <div className="flex-1 pr-1 h-full overflow-y-auto no-scrollbar min-h-0">
+            {children}
+        </div>
+    );
 }
 
 export function TabRight({ children }: { children: ReactNode }) {
-    return <div className="w-full mr-2">{children}</div>;
+    return (
+        <div className="flex-1 mr-2 h-full overflow-y-auto no-scrollbar min-h-0">
+            {children}
+        </div>
+    );
 }
